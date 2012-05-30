4aa2038c7af28ce6ab33391cac89ce9c4a90ca68
YA <ya@yaprak.(none)>
2012-05-30 16:44:36 +0300

    Deleted.

--- src/StreamSink.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface StreamSink {
-    final String NEWLINE = "\r\n";
-    void write(byte[] b) throws Exception;
-    void initialize(List<String> headers);
-    boolean isInitialized();
-}
